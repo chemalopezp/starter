7c61f5e5ccf11eb216033b73a8f19dcd85c0e395
Chema Lopez <chema.lopezp@gmail.com>
2024-04-05 14:35:41 -0700

    trpc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./web/index.html", "./web/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
